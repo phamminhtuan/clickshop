53c2b6d6d5039aac519bebf7de3c88f4200406be
phamminhtuan <pminhtuan89@gmail.com>
2014-07-07 22:35:23 +0700

    update core

--- Database/ClickShop/Tables/dbo.Users.sql
+++ Database/ClickShop/Tables/dbo.Users.sql
@@ -1,6 +1,13 @@
 CREATE TABLE [dbo].[Users]
 (
-[Id] [uniqueidentifier] NULL,
-[UserName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Id] [uniqueidentifier] NOT NULL,
+[UserName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[PassWord] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FullName] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Phone] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[IsActive] [bit] NOT NULL CONSTRAINT [DF_Users_IsActive] DEFAULT ((1))
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Users] ADD 
+CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 GO
--- Source/Clickshop.UI/Program.cs
+++ Source/Clickshop.UI/Program.cs
@@ -16,7 +16,7 @@ namespace Clickshop.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Mainform());
         }
     }
 }
